69d58f3727f5361fa4af59d03141ff8eeefde67e
Fredrick Irubor <56285185+fr3drick@users.noreply.github.com>
2022-12-29 13:54:27 +0100

    Update CustomerController.java

--- src/main/java/com/fred/store/customer/CustomerController.java
+++ src/main/java/com/fred/store/customer/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1")
 @RestController
 public class CustomerController {
 
